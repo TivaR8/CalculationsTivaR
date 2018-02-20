7b8a4eb3bc2d0d9354940527d0724c766dc16065
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-02-20 11:50:52 -0500

    Initial commit

---
+++ CalculationsTivaR/CalculationForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace CalculationsTivaR
+{
+    partial class frmCalculations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuestion1 = new System.Windows.Forms.Button();
+            this.btnQuestion2 = new System.Windows.Forms.Button();
+            this.btnQuestion3 = new System.Windows.Forms.Button();
+            this.btnQuestion4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnQuestion1
+            // 
+            this.btnQuestion1.Location = new System.Drawing.Point(31, 28);
+            this.btnQuestion1.Name = "btnQuestion1";
+            this.btnQuestion1.Size = new System.Drawing.Size(75, 23);
+            this.btnQuestion1.TabIndex = 0;
+            this.btnQuestion1.Text = "button1";
+            this.btnQuestion1.UseVisualStyleBackColor = true;
+            // 
+            // btnQuestion2
+            // 
+            this.btnQuestion2.Location = new System.Drawing.Point(31, 77);
+            this.btnQuestion2.Name = "btnQuestion2";
+            this.btnQuestion2.Size = new System.Drawing.Size(75, 23);
+            this.btnQuestion2.TabIndex = 1;
+            this.btnQuestion2.Text = "button2";
+            this.btnQuestion2.UseVisualStyleBackColor = true;
+            // 
+            // btnQuestion3
+            // 
+            this.btnQuestion3.Location = new System.Drawing.Point(31, 119);
+            this.btnQuestion3.Name = "btnQuestion3";
+            this.btnQuestion3.Size = new System.Drawing.Size(75, 23);
+            this.btnQuestion3.TabIndex = 2;
+            this.btnQuestion3.Text = "button3";
+            this.btnQuestion3.UseVisualStyleBackColor = true;
+            // 
+            // btnQuestion4
+            // 
+            this.btnQuestion4.Location = new System.Drawing.Point(31, 164);
+            this.btnQuestion4.Name = "btnQuestion4";
+            this.btnQuestion4.Size = new System.Drawing.Size(75, 23);
+            this.btnQuestion4.TabIndex = 3;
+            this.btnQuestion4.Text = "button4";
+            this.btnQuestion4.UseVisualStyleBackColor = true;
+            // 
+            // frmCalculations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnQuestion4);
+            this.Controls.Add(this.btnQuestion3);
+            this.Controls.Add(this.btnQuestion2);
+            this.Controls.Add(this.btnQuestion1);
+            this.Name = "frmCalculations";
+            this.Text = "Calculations By Tiva Rait";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuestion1;
+        private System.Windows.Forms.Button btnQuestion2;
+        private System.Windows.Forms.Button btnQuestion3;
+        private System.Windows.Forms.Button btnQuestion4;
+    }
+}
+
---
+++ CalculationsTivaR/CalculationForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculationsTivaR
+{
+    public partial class frmCalculations : Form
+    {
+        public frmCalculations()
+        {
+            InitializeComponent();
+        }
+    }
+}
