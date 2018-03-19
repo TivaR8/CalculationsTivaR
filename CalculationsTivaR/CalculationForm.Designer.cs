4766621c0f51e5b5a44d4e3a1960ac7c16379e24
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-03-19 13:38:45 -0400

    Finished Product
    Even includes screenshots

--- CalculationsTivaR/CalculationForm.Designer.cs
+++ CalculationsTivaR/CalculationForm.Designer.cs
@@ -32,6 +32,10 @@
             this.btnQuestion2 = new System.Windows.Forms.Button();
             this.btnQuestion3 = new System.Windows.Forms.Button();
             this.btnQuestion4 = new System.Windows.Forms.Button();
+            this.lblAnswer1 = new System.Windows.Forms.Label();
+            this.lblAnswer2 = new System.Windows.Forms.Label();
+            this.lblAnswer3 = new System.Windows.Forms.Label();
+            this.lblAnswer4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnQuestion1
@@ -40,8 +44,9 @@
             this.btnQuestion1.Name = "btnQuestion1";
             this.btnQuestion1.Size = new System.Drawing.Size(75, 23);
             this.btnQuestion1.TabIndex = 0;
-            this.btnQuestion1.Text = "button1";
+            this.btnQuestion1.Text = "5+2^3 =";
             this.btnQuestion1.UseVisualStyleBackColor = true;
+            this.btnQuestion1.Click += new System.EventHandler(this.btnQuestion1_Click);
             // 
             // btnQuestion2
             // 
@@ -49,8 +54,9 @@
             this.btnQuestion2.Name = "btnQuestion2";
             this.btnQuestion2.Size = new System.Drawing.Size(75, 23);
             this.btnQuestion2.TabIndex = 1;
-            this.btnQuestion2.Text = "button2";
+            this.btnQuestion2.Text = "4/2 + 5 =";
             this.btnQuestion2.UseVisualStyleBackColor = true;
+            this.btnQuestion2.Click += new System.EventHandler(this.btnQuestion2_Click);
             // 
             // btnQuestion3
             // 
@@ -58,8 +64,9 @@
             this.btnQuestion3.Name = "btnQuestion3";
             this.btnQuestion3.Size = new System.Drawing.Size(75, 23);
             this.btnQuestion3.TabIndex = 2;
-            this.btnQuestion3.Text = "button3";
+            this.btnQuestion3.Text = "3 + 4 * 2 =";
             this.btnQuestion3.UseVisualStyleBackColor = true;
+            this.btnQuestion3.Click += new System.EventHandler(this.btnQuestion3_Click);
             // 
             // btnQuestion4
             // 
@@ -67,14 +74,55 @@
             this.btnQuestion4.Name = "btnQuestion4";
             this.btnQuestion4.Size = new System.Drawing.Size(75, 23);
             this.btnQuestion4.TabIndex = 3;
-            this.btnQuestion4.Text = "button4";
+            this.btnQuestion4.Text = "7 - 3 + 2 =";
             this.btnQuestion4.UseVisualStyleBackColor = true;
+            this.btnQuestion4.Click += new System.EventHandler(this.btnQuestion4_Click);
+            // 
+            // lblAnswer1
+            // 
+            this.lblAnswer1.AutoSize = true;
+            this.lblAnswer1.Location = new System.Drawing.Point(176, 33);
+            this.lblAnswer1.Name = "lblAnswer1";
+            this.lblAnswer1.Size = new System.Drawing.Size(51, 13);
+            this.lblAnswer1.TabIndex = 4;
+            this.lblAnswer1.Text = "Answer 1";
+            // 
+            // lblAnswer2
+            // 
+            this.lblAnswer2.AutoSize = true;
+            this.lblAnswer2.Location = new System.Drawing.Point(176, 82);
+            this.lblAnswer2.Name = "lblAnswer2";
+            this.lblAnswer2.Size = new System.Drawing.Size(51, 13);
+            this.lblAnswer2.TabIndex = 5;
+            this.lblAnswer2.Text = "Answer 2";
+            // 
+            // lblAnswer3
+            // 
+            this.lblAnswer3.AutoSize = true;
+            this.lblAnswer3.Location = new System.Drawing.Point(176, 124);
+            this.lblAnswer3.Name = "lblAnswer3";
+            this.lblAnswer3.Size = new System.Drawing.Size(51, 13);
+            this.lblAnswer3.TabIndex = 6;
+            this.lblAnswer3.Text = "Answer 3";
+            // 
+            // lblAnswer4
+            // 
+            this.lblAnswer4.AutoSize = true;
+            this.lblAnswer4.Location = new System.Drawing.Point(176, 169);
+            this.lblAnswer4.Name = "lblAnswer4";
+            this.lblAnswer4.Size = new System.Drawing.Size(51, 13);
+            this.lblAnswer4.TabIndex = 7;
+            this.lblAnswer4.Text = "Answer 4";
             // 
             // frmCalculations
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblAnswer4);
+            this.Controls.Add(this.lblAnswer3);
+            this.Controls.Add(this.lblAnswer2);
+            this.Controls.Add(this.lblAnswer1);
             this.Controls.Add(this.btnQuestion4);
             this.Controls.Add(this.btnQuestion3);
             this.Controls.Add(this.btnQuestion2);
@@ -82,6 +130,7 @@
             this.Name = "frmCalculations";
             this.Text = "Calculations By Tiva Rait";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -91,6 +140,10 @@
         private System.Windows.Forms.Button btnQuestion2;
         private System.Windows.Forms.Button btnQuestion3;
         private System.Windows.Forms.Button btnQuestion4;
+        private System.Windows.Forms.Label lblAnswer1;
+        private System.Windows.Forms.Label lblAnswer2;
+        private System.Windows.Forms.Label lblAnswer3;
+        private System.Windows.Forms.Label lblAnswer4;
     }
 }
 
--- CalculationsTivaR/CalculationForm.cs
+++ CalculationsTivaR/CalculationForm.cs
@@ -7,6 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 08-03-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #6 - Calculations
+ * This program has a bunch of buttons with calculations on them...
+ * when you press the button they display the answer.
+*/
 
 namespace CalculationsTivaR
 {
@@ -15,6 +23,38 @@ namespace CalculationsTivaR
         public frmCalculations()
         {
             InitializeComponent();
+            lblAnswer1.Hide();
+            lblAnswer2.Hide();
+            lblAnswer3.Hide();
+            lblAnswer4.Hide();
+        }
+
+        private void btnQuestion1_Click(object sender, EventArgs e)
+        {
+            // Show and calculate the answer
+            lblAnswer1.Text = Convert.ToString(5 + Math.Pow(2, 3));
+            lblAnswer1.Show();
+        }
+
+        private void btnQuestion2_Click(object sender, EventArgs e)
+        {
+            // Show and calculate the answer
+            lblAnswer2.Text = Convert.ToString(4 / 2 + 5);
+            lblAnswer2.Show();
+        }
+
+        private void btnQuestion3_Click(object sender, EventArgs e)
+        {
+            // Show and calculate the answer
+            lblAnswer3.Text = Convert.ToString(3 + 4 * 2);
+            lblAnswer3.Show();
+        }
+
+        private void btnQuestion4_Click(object sender, EventArgs e)
+        {
+            // Show and calculate the answer
+            lblAnswer4.Text = Convert.ToString(7 - 3 + 2);
+            lblAnswer4.Show();
         }
     }
 }
